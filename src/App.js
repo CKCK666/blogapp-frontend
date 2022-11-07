4b2659c90e6eb0cea1675f375b404870ffea0c21
vishnu ck <vishnuck464@gmail.com>
2022-11-07 15:53:30 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
    
       <TopBar />
       <Switch>
-        <Route exact path="/">
+        <Route exact path="https://blogapp-frontend.onrender.com">
           <Home />
         </Route>
         <Route path="/register">{user ? <Home /> : <Register />}</Route>
