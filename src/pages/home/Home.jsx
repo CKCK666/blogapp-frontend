ef70d274b9ec607b750c8bddb5f098c59e425a0b
vishnu ck <vishnuck464@gmail.com>
2022-11-14 22:07:22 +0530

    final

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,8 +5,9 @@ import { host } from "../../constants";
 import "./home.css";
 import axios from "axios";
 import { useLocation } from "react-router";
-
+import Loader from "../../components/Loader";
 export default function Home() {
+  const [loader,setLoader]=useState(false)
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
   const arrayPosts=[]
@@ -14,9 +15,11 @@ export default function Home() {
   useEffect(() => {
     
     const fetchPosts = async () => {
+      setLoader(true)
       const res = await axios.get(`${host}/posts` + search);
-      
+      setLoader(false)
       setPosts(res.data);
+      
      
     };
     fetchPosts();
@@ -26,8 +29,10 @@ export default function Home() {
     <>
       <Header />
       <div className="home">
-     
-        <Posts posts={posts} />
+     {loader ? <Loader/> :
+     <Posts posts={posts} />
+     }
+       
        
       </div>
     </>
